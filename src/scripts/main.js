12fd59e0484bde7e8065e8d3dc776f33876c9e8c
shsazib <shsazibbhuiyan892939@gmail.com>
2024-03-09 10:12:38 +0600

    add protfolio, blog, single-blog page, pricing and contact section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 AOS.init();
+
+//selecting all required elements
+const filterItem = document.querySelector(".portfolio_tabs");
+const filterImg = document.querySelectorAll(".gallery .image");
+
+window.onload = ()=>{
+  filterItem.onclick = (selectedItem)=>{
+    if(selectedItem.target.classList.contains("portfolio_tab")){
+      filterItem.querySelector(".active").classList.remove("active");
+      selectedItem.target.classList.add("active");
+      let filterName = selectedItem.target.getAttribute("data-name");
+      filterImg.forEach((image) => {
+        let filterImges = image.getAttribute("data-name");
+        if((filterImges == filterName) || (filterName == "all")){
+          image.classList.remove("hide"); 
+          image.classList.add("show"); 
+        }else{
+          image.classList.add("hide"); 
+          image.classList.remove("show"); 
+        }
+      });
+    }
+  }
+  for (let i = 0; i < filterImg.length; i++) {
+    filterImg[i].setAttribute("onclick", "preview(this)");
+  }
+}
+
